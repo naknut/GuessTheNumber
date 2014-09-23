00c741c3a0919dd93de24efac220d060d52bd0dd
Marcus Isaksson <mail@naknut.se>
2014-09-23 11:49:45 +0200

    Added error handling for args

--- DumbClient/src/DumbClient.java
+++ DumbClient/src/DumbClient.java
@@ -10,7 +10,18 @@ import java.util.Scanner;
  */
 public class DumbClient {
     public static void main(String[] args) throws SocketException {
-        InetSocketAddress address = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+        if(args.length < 2) {
+            System.out.println("Enter hostname and port as agruments");
+            System.exit(1);
+        }
+        int port = 0;
+        try {
+             port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Argument" + args[1] + " must be an integer.");
+            System.exit(1);
+        }
+        InetSocketAddress address = new InetSocketAddress(args[0], port);
         byte[] inBuffer = new byte[7];
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
